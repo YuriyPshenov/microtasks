e4d5241a5982db06409fce77655d9c508893320a
YuriyPshenov <yuriypshenov@gmail.com>
2022-10-17 15:28:06 +0300

    updating microtask-3

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import {Body} from "./microtask-1/Body";
 import {Footer} from "./microtask-1/Footer";
 import {NewComponent} from "./microtask-2/NewComponent";
 import {NewComponent2} from "./microtask-2/NewComponent-2";
+import {Button} from "./microtask-3/Button";
 
 
 
@@ -33,15 +34,29 @@ function App() {
         {manufacturer: 'Audi', model: 'rs6'},
     ]
 
+    const Button1Foo = (subscriber: string, age: number, adress: string) => {
+        console.log(subscriber, age, adress)
+    }
+
+    const Button2Foo = (subscriber: string) => {
+        console.log(subscriber)
+    }
+
+    const Button3Foo = () => {
+        console.log('Im stupid button')
+    }
 
     return (
-        <>
+        <div className='App-Wrapper'>
             <Header titleForHeader={'NewHeader'}/>
             <Body titleForBody={'NewBody'}/>
             <Footer titleForFooter={'NewFooter'}/>
             <NewComponent students={students}/>
             <NewComponent2 topCars={topCars1} />
-        </>
+            <Button name={'MyYoutubeChanel-1'} callback={() => Button1Foo('Im vadim', 21, 'live in minsk')}/>
+            <Button name={'MyYoutubeChanel-2'} callback={() => Button2Foo('Im gosha')}/>
+            <Button name={'Im stupid button'} callback={Button3Foo}/>
+        </div>
     );
 }
 
